0ca84a39a93f4a3a85f9deff13b055f9a8880541
HERMANN3712 <di.hermann@wanadoo.fr>
2026-02-04 18:57:42 +0100

    refactoring

--- GildedRoseTests/ApprovalTest.cs
+++ GildedRoseTests/ApprovalTest.cs
@@ -16,7 +16,7 @@ public class ApprovalTest
     [Fact]
     public Task Foo()
     {
-        Item[] items = { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+        Item[] items = { new() { Name = "foo", SellIn = 0, Quality = 0 } };
         GildedRose app = new GildedRose(items);
         app.UpdateQuality();
         
